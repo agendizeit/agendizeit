e0d7afbbf0cf9fe4a6df610a8daa763d9d2b750c
skittles <julia.heath@me.com>
2018-06-24 17:00:40 -0700

    Trying to add a discussion box, linked it to Admin but not sure if that's necessary if it should be editabel by users, not just Admin

--- meetingdiscuss/models.py
+++ meetingdiscuss/models.py
@@ -7,4 +7,6 @@ class AgendaItem(models.Model):
     topic = models.CharField(max_length=150)
     text = models.TextField()
 
-
+class ItemDiscussion(models.Model):
+    discussion = models.CharField(max_length=255)
+    text = models.TextField()
